82604dc96878f90be5fcee745aa29f4316794a87
Brian Stolte <brian.stolte@gmail.com>
2015-10-14 05:32:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kumquat::Application.config.secret_key_base = '9902de1ff8d17ac4a12a8fbff9ad4a6d281d6bec6b6f4931dc77003e3acec77947d31038016268adda02970941a38bb31532b088fdaa727b6cf50b1989e042bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Kumquat::Application.config.session_store :cookie_store, key: '_kumquat_session'
